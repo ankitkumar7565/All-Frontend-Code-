5d3f3c4ff4062c8c7f90290a053a2080e2e8fd4e
Ankit kumar <98960172+ankitkumar7565@users.noreply.github.com>
2026-01-11 15:44:03 +0530

    Splash
    package com.example.managementtask
    
    import android.content.Intent
    import android.os.Bundle
    import android.os.Handler
    import android.os.Looper
    import androidx.activity.enableEdgeToEdge
    import androidx.appcompat.app.AppCompatActivity
    import androidx.core.view.ViewCompat
    import androidx.core.view.WindowInsetsCompat
    import com.example.managementtask.Data.SessionManager
    
    class SplashScreen : AppCompatActivity() {
        override fun onCreate(savedInstanceState: Bundle?) {
            super.onCreate(savedInstanceState)
            enableEdgeToEdge()
            setContentView(R.layout.activity_splash_screen)
            val session = SessionManager(this)
    
            Handler(Looper.getMainLooper()).postDelayed({
    
                if (session.isLoggedIn()) {
                    // ✅ User already logged in
                    startActivity(Intent(this, Dashboard::class.java))
                } else {
                    // ✅ New user
                    startActivity(Intent(this, Registration::class.java))
                }
    
                finish()
            }, 2000)
    
        }
    }

---
+++ SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.managementtask
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.managementtask.Data.SessionManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        val session = SessionManager(this)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            if (session.isLoggedIn()) {
+                // ✅ User already logged in
+                startActivity(Intent(this, Dashboard::class.java))
+            } else {
+                // ✅ New user
+                startActivity(Intent(this, Registration::class.java))
+            }
+
+            finish()
+        }, 2000)
+
+    }
+}
